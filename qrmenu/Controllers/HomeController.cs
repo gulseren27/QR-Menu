941105a1b4b5932ee6af48a51c195c7519c2d8d9
Gülseren Zamir <86431567+gulseren27@users.noreply.github.com>
2024-04-06 23:12:29 +0300

    Qr Menu
    Restaurant and Cafe Qr Menu

---
+++ qrmenu/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace qrmenu.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+        public ActionResult Category()
+        {
+
+            return View();
+        }
+
+        public ActionResult MenuList()
+        {
+            return View();
+        }
+        public ActionResult MenuDetail()
+        {
+            return View();
+        }
+    }
+}
